75dbb8aa920d3a3260ecdde5a207bd201c899aae
GTM35 <035guuh@gmail.com>
2024-02-21 11:36:28 -0300

    Finalizando exercícios

---
+++ multiplicacao.ts
@@ -0,0 +1,7 @@
+function multiplicar(numero1: number, numero2: number): number {
+  return numero1 * numero2;
+}
+
+multiplicar(2, 2);
+
+console.log(typeof multiplicar(2, 2));
---
+++ saudacoes.ts
@@ -0,0 +1,6 @@
+function saudacoes(nome: string): string {
+  const saudacao: string = `Olá ${nome}`;
+  return saudacao;
+}
+
+console.log(saudacoes("Gustavo"));
